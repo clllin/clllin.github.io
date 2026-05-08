e1f6392cc53cdc3277e454aea186283a9ceaf557
clllin <13681217107@163.com>
2026-05-08 11:17:51 +0800

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,24 @@
+// 这是你的全站唯一数据中心
+const ALL_PROJECTS = [
+    {
+        id: "camping",
+        title: "内蒙古露营小镇",
+        date: "2024-05", // 靠上的会显示在首页
+        category: "品牌全案",
+        cover: "封面.jpg", // 既是缩略图，也是详情页头图
+        url: "coffee.html",
+        description: "针对内蒙古文旅特色的奇幻星球策划...",
+        details: ['详情1.jpg', '详情2.jpg', '详情3.jpg']
+    },
+    {
+        id: "tech",
+        title: "未来实验室",
+        date: "2024-04",
+        category: "视觉设计",
+        cover: "tech_cover.jpg",
+        url: "tech.html",
+        description: "探索数字艺术的边界...",
+        details: ['t1.jpg', 't2.jpg']
+    }
+    // 以后增加项目，只需按格式把这段代码复制粘贴到最上面
+];
