9a209f274f5ece76ac04b01f9071164d17d43a30
clllin <13681217107@163.com>
2026-06-10 09:24:40 +0800

    Update projects.js

--- projects.js
+++ projects.js
@@ -3,7 +3,7 @@
  * 提示：新项目请写在数组的最前面，这样首页会自动显示最新作品
  */
 const ALL_PROJECTS = [
-{
+    {
         id: "kaigang",
         title: "开杠招商手册设计",
         date: "2021-04",
@@ -13,7 +13,7 @@ const ALL_PROJECTS = [
         role: "主设计师",
         cover: "images/kaigang/开杠封面.jpg",
         url: "project-kaigang.html",
-        description: `“开杠招商手册”设计本手册设计以极具张力的“国潮绿”与“中国红”为主基调，将传统麻将文化与现代潮流插画趣味融合，打造兼具文化底蕴与前卫网格系统的视觉排版。整体设计通过大块面的色彩碰撞与精致的图表系统，在张扬品牌年轻活力的同时，清晰传递了餐饮品牌的商业模式与加盟优势。`,
+        description: `“开杠招商手册”设计本手册设计以极极具张力的“国潮绿”与“中国红”为主基调，将传统麻将文化与现代潮流插画趣味融合，打造兼具文化底蕴与前卫网格系统的视觉排版。整体设计通过大块面的色彩碰撞与精致的图表系统，在张扬品牌年轻活力的同时，清晰传递了餐饮品牌的商业模式与加盟优势。`,
         details: [
             "images/kaigang/开杠封面.jpg",
             "images/kaigang/开杠1.jpg",
@@ -42,6 +42,25 @@ const ALL_PROJECTS = [
             "images/atpc/农贸促手册4.jpg",
             "images/atpc/农贸促手册5.jpg",
             "images/atpc/农贸促手册6.jpg"
+        ]
+    },
+    {
+        id: "rudolf",
+        title: "鲁道夫项目设计",
+        date: "2026-06",
+        category: "印刷物料设计 / Print & Layout",
+        client: "鲁道夫品牌方",
+        services: "画册排版, 物料视觉设计, 印刷工艺规划",
+        role: "主设计师",
+        cover: "images/rudolf/鲁道夫封面.jpg",
+        url: "project-rudolf.html",
+        description: `“鲁道夫”印刷物料设计围绕品牌空气能净化的核心视觉资产展开，通过严谨的现代版式律动与大面积留白，赋予画面高级、洗练的视觉质感。在信息层级上进行了精细化的梳理，确保商业核心内容的逻辑性传达，同时结合富有层次的网格规范，使整体画册兼具极高的审美价值与阅读体验。`,
+        details: [
+            "images/rudolf/鲁道夫封面.jpg",
+            "images/rudolf/鲁道夫1.jpg",
+            "images/rudolf/鲁道夫2.jpg",
+            "images/rudolf/鲁道夫3.jpg",
+            "images/rudolf/鲁道夫4.jpg"
         ]
     },
     {
@@ -83,8 +102,8 @@ const ALL_PROJECTS = [
             'images/camping/详情5.jpg',
             'images/camping/详情6.jpg'
         ]
-},
-{
+    },
+    {
         id: "elementsclub",
         title: "Elements Club 品牌视觉系统设计",
         date: "2026-05",
@@ -108,5 +127,5 @@ const ALL_PROJECTS = [
             "images/elementsclub/elementsclub10.jpg",
             "images/elementsclub/elementsclub11.png"
         ]
-    },
-]; //
+    }
+];
